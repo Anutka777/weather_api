b24caa9c9cb6a78e1dd908c05cd138834e2585b5
Anna Baburina <4nna.baburina@gmail.com>
2022-06-05 20:20:08 +0500

    DEV Implement min and max functionality

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,12 +2,33 @@ class WeatherController < ApplicationController
   before_action :collect_data
 
   def current
-    render json: @weather.first[:temperature]
+    render json: @weather.first
+  end
+
+  def historical
+    render json: @weather
+  end
+
+  def historical_max
+    @weather = sort_by_temperature
+    max = @weather.last
+    render json: max
+  end
+
+  def historical_min
+    @weather = sort_by_temperature
+    min = @weather.first
+    render json: min
   end
   
   private
   
   def collect_data
+    # TODO: Date condition 
     @weather = Weather.get_weather
+  end
+
+  def sort_by_temperature
+    @weather.sort_by { |item| item[:temperature]['Value']}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'current', to: 'weather#current'
+  get 'weather/historical', to: 'weather#historical'
+  get 'weather/historical/max', to: 'weather#historical_max'
+  get 'weather/historical/min', to: 'weather#historical_min'
 
   root to: 'weather#current'
 end
