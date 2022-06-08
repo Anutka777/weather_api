d67de5cd50086168e0daa2755fad6afe8cf5983c
Anna Baburina <4nna.baburina@gmail.com>
2022-06-09 04:54:25 +0500

    DEV Add epoch to collected data + clean up

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -2,25 +2,28 @@ class Weather < ApplicationRecord
   require 'net/http'
   require 'json'
 
-  def self.call_api
-    uri = URI("http://dataservice.accuweather.com/currentconditions/v1/290714/historical/24?apikey=IVbDrteOShnZzYss4kSDoLZdGpDf96Z9")
-    data = Net::HTTP.get(uri)
-  end
-
   def self.collect_data
     # Actual behavour
-    # data_json = self.call_api
+    # response = self.call_api
 
     # Introduce dummy json to minimize API's calls count
-    data_json = File.read(Rails.root.join "app", "models", "data.json")
-    data = JSON.parse(data_json)
-
+    response = File.read(Rails.root.join "app", "models", "data.json")
+    
     # Filter irrelevant info
+    data = JSON.parse(response)
     data.collect do |item|
       Hash[
+        epoch: item ['EpochTime'],
         time: item['LocalObservationDateTime'],
         temperature: item['Temperature']['Metric']
       ]
     end
+  end
+
+  private
+
+  def self.call_api
+    uri = URI("http://dataservice.accuweather.com/currentconditions/v1/290714/historical/24?apikey=IVbDrteOShnZzYss4kSDoLZdGpDf96Z9")
+    Net::HTTP.get(uri)
   end
 end
